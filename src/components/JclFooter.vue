5c258b2b9fb8ed15b777d6aa25aebfd048b87873
jinusean <lee46james@gmail.com>
2019-01-06 20:59:21 +0800

    logos

---
+++ src/components/JclFooter.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="jcl-footer">
+    <div class="jcl-footer__container">
+      <a class="jcl-footer__email" href="mailto:atjameslee@gmail.com" alt="email me">atjameslee@gmail.com</a>
+      <ul class="contact-links">
+        <li
+          v-for="(value, key) in links"
+          :key="key"
+          class="contact-links__link">
+          <a
+            :href="value"
+            target="_blank">
+            <img
+              :src="`/logos/${key}.svg`"
+              :alt="key">
+          </a>
+          <!--<span class="contact-links__link__text">-->
+          <!--{{ key }}-->
+          <!--</span>-->
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JclFooter',
+  computed: {
+    links() {
+      return {
+        // gmail: 'mailto:atjameslee@gmail.com',
+        gitlab: 'https://gitlab.com/jinusean',
+        instagram: 'https://instagram.com/atjameslee'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.jcl-footer {
+  overflow: hidden;
+  background: $gray-100;
+
+  .jcl-footer__container {
+    display: flex;
+    flex-flow: wrap;
+    align-items: center;
+    justify-content: space-between;
+    width: 1100px;
+    max-width: 100%;
+    padding: 1em 2em;
+    margin: 0 auto;
+  }
+
+  .jcl-footer__email {
+    margin: 1em 0;
+    font-weight: bold;
+  }
+
+  .contact-links {
+    display: flex;
+    justify-content: center;
+    margin: 0;
+
+    .contact-links__link {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      transition: $transition;
+
+      &:not(:last-child) {
+        margin-right:1em;
+      }
+
+      img {
+        width: 2em;
+        height: 2em;
+      }
+
+      .contact-links__link__text {
+        position: absolute;
+        bottom: -1em;
+        //color: $link-color;
+        text-transform: capitalize;
+        opacity: 0;
+        transition: $transition;
+      }
+    }
+
+    .contact-links__link:hover {
+      a {
+        opacity: 0.5;
+      }
+
+      .contact-links__link__text {
+        opacity: 1;
+      }
+    }
+  }
+}
+</style>
