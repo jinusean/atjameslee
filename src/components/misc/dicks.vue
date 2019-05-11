b846fd5b586c400c701bfe686919b8ecbd60948c
jinusean <lee46james@gmail.com>
2019-05-11 20:27:26 +0700

    readded d

---
+++ src/components/misc/dicks.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="full-screen">
+    <dicks />
+  </div>
+</template>
+<script>
+import Dicks from '~/components/misc/Dicks'
+
+export default {
+  components: { Dicks }
+}
+</script>
